a849fa7743d3f74ae0cdbc94d0ef351048bba743
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-11-20 17:28:54 +1100

    t2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module provides the floor function.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Takes a float n as argument,
+    and returns the floor of the float as an int.
+    """
+    return int(n)
