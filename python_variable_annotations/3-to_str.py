e131568cdcd49e88acb64f19529cabed24a70e1c
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-11-20 17:32:09 +1100

    t3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module provides the type-annotated function to_str.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float n as argument,
+    and returns the string representation of the float.
+    """
+    return str(n)
