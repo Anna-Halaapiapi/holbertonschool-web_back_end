3daf316fe2a23dda532d49ea34b3f6c8662f4031
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-11-20 19:25:34 +1100

    pycodestyle compliance

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,8 +5,18 @@ This module provides the type-annotated function to_kv.
 from typing import List, Union, Tuple
 
 
-# # for version < 3.9: need to use typing library for all containers and also for Union
-# def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+# for v < 3.9: need to use typing library for all containers and also for Union
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Args:
+    k: a string
+    v: an int or float
+    Returns a tuple of k and v squared (annotated as float).
+    """
+    return (k, v ** 2)
+
+# # for v 3.9: don't need typing library for containers, still need for Union
+# def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
 #     """
 #     Args:
 #     k: a string
@@ -14,16 +24,6 @@ from typing import List, Union, Tuple
 #     Returns a tuple of k and v squared (annotated as float).
 #     """
 #     return(k, v ** 2)
-
-# for version 3.9: don't need to use typing library for containers, but still need Union
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
-    """
-    Args:
-    k: a string
-    v: an int or float
-    Returns a tuple of k and v squared (annotated as float).
-    """
-    return(k, v ** 2)
 
 # # for version 3.10: replaces typing.Union with pipeline
 # def to_kv(k: str, v: int | float) -> tuple[str, float]:
