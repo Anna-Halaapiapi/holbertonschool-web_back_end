34b49272774924962d03f7992153a2b4e4be70d5
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-11-20 18:01:36 +1100

    t6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module provides the type-annotated function sum_mixed_list.
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]] ) -> float:
+    """
+    Takes a list of ints and floats,
+    and returns their sum as a float.
+    """
+    return sum(mxd_lst)
