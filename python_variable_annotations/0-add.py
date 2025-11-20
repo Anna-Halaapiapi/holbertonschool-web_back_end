b5dd1056de0e03d9b261996be39b883cad547b9c
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-11-20 17:15:10 +1100

    t0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module provides the add function,
+which adds two floats and returns the sum.
+"""
+
+
+def add(a: float, b: float):
+    """
+    This function adds floats a & b,
+    returning the sum.
+    """
+    return a + b
