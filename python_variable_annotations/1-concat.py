f1c56a55684374a66891b1cf275b06a4d1ed954e
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-11-20 17:21:58 +1100

    t1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module provides the concat function.
+"""
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatentates two strings and returns the result.
+    """
+    return str1 + str2
