6b72dc2fab066dde508dc09a960216fccf8ad4da
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-11-24 21:57:20 +1100

    t1 - refactor logic

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
+"""
+this module provides the type-annotaed asynchronous coroutine, wait_n.
+"""
 
 import asyncio
 import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
-
-"""
-this module provides the type-annotaed asynchronous coroutine, wait_n.
-"""
 
 
 async def wait_n(n: int, max_delay: int) -> typing.List[float]:
@@ -14,8 +13,16 @@ async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     runs wait_random n times with the specified max_delay
     returns the list of all the delays.
     """
-    delays = []
+    # build list of coroutine objects
+    tasks = []
     for num in range(n):
-        delay = await wait_random(max_delay)
+        task = wait_random(max_delay)
+        tasks.append(task)
+
+    delays = []
+    # start all coros same time, get results back in completion order via loop
+    for coroutine in asyncio.as_completed(tasks):
+        delay = await coroutine
         delays.append(delay)
+
     return delays
