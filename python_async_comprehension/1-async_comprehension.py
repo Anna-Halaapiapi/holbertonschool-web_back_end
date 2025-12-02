fdf63b4918082424e8998fcd024e7eaeed00d697
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-12-02 12:07:30 +1100

    t1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+this module provides the async_comprehension function.
+"""
+
+import asyncio
+import typing
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> typing.List[float]:
+    """
+    collects 10 random numbers using an async comprehensing
+    over async_generator, then return the 10 random numbers.
+    """
+    nums = []
+    async for i in async_generator():
+        nums.append(i)
+    return nums
