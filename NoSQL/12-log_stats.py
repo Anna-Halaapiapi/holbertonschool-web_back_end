2f7a92327e3dc3d4a92119dbb1afefdbe18cb4d0
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-12-10 20:13:16 +1100

    t12 - wrap code + pycodestyle

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,30 +5,26 @@ script that provides stats about Nginx logs stored in MongoDB
 
 from pymongo import MongoClient
 
-# create the connection between Python script & MongoDB server
-client = MongoClient("mongodb://127.0.0.1:27017")
-# select 'nginx' collection from db 'logs'
-nginx_collection = client.logs.nginx
+if __name__ == "__main__":
 
-# print total num of docs in nginx collection
-print(f"{nginx_collection.count_documents({})} logs")
+    # create the connection between Python script & MongoDB server
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    # select 'nginx' coll from db 'logs'
+    coll = client.logs.nginx
 
-# # get number of each request
-# requests = {
-# "GET": nginx_collection.count_documents({"method": "GET"}),
-# "POST": nginx_collection.count_documents({"method": "POST"}),
-# "PUT": nginx_collection.count_documents({"method": "PUT"}),
-# "PATCH": nginx_collection.count_documents({"method": "PATCH"}),
-# "DELETE": nginx_collection.count_documents({"method": "DELETE"})
-# }
+    # print total num of docs in nginx coll
+    print(f"{coll.count_documents({})} logs")
 
-print("Methods:")
-# for key, value in requests.items():
-#     print(f"\tmethod {key}: {value}")
+    # get and print each stat
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print(
+            f"\tmethod {method}: "
+            f"{coll.count_documents({'method': method})}"
+        )
 
-# get and print each stat
-for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    print(f"\tmethod {method}: {nginx_collection.count_documents({'method': method})}")
-
-# print num of docs where method is GET and path is /status
-print(f"{nginx_collection.count_documents({'method': 'GET', 'path': '/status'})} status check")
+    # print num of docs where method is GET and path is /status
+    print(
+        f"{coll.count_documents({'method': 'GET', 'path': '/status'})}"
+        f" status check"
+        )
