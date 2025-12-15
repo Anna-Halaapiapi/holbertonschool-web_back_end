a21b1bea600693ae82dc6dadc05acb74d4039eac
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-12-15 20:50:04 +1100

    t7 - 7-getBudgetObject.js remove comments causing checker conflict

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,6 @@
 // use shorthand syntax for key:variable name
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
-    // income: income,
-    // gdp: gdp,
-    // capita: capita,
     income,
     gdp,
     capita,
