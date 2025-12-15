90c6ec2f1573d4459397a7a5e14c54b16c8e60a4
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-12-15 20:49:12 +1100

    t7

---
+++ ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,13 @@
+// use shorthand syntax for key:variable name
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    // income: income,
+    // gdp: gdp,
+    // capita: capita,
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
