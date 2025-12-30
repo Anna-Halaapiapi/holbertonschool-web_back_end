00edc4c1d23c786b5ec175af1c8d72d321befe99
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-12-30 21:57:36 +1100

    t5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray (length, position, value) {
+  // returns a new ArrayBuffer with an Int8 value at a specific position
+  const buffer = new ArrayBuffer(length); //create array buffer/memory
+  const view = new DataView(buffer); // create a Dataview to access buffer
+  try {
+  view.setInt8(position, value); // write to the buffer using the dataview
+  } catch (error) {
+    throw new Error('Position outside range');
+  }
+  return buffer;
+}
