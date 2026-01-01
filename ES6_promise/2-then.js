957511abbcca53c68e22c7c78533ca97182bfc46
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-01-01 20:32:25 +1100

    t2

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+export default function handleResponseFromAPI(promise) {
+  // uses three handlers to handle promise
+    return promise
+      .then(() => { // handles resolved promise
+        return {status: 200, body: 'Success'};
+      })
+      .catch(error => { // handles rejected promise
+        return Error();
+      })
+      .finally(() => { // for every resolution, logs msg to console
+        console.log('Got a response from the API');
+      });
+}
