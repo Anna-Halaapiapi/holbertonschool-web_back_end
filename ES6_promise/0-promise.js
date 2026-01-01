5a295f543f0bdb7237b420500379fabed72cb2cb
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-01-01 19:41:57 +1100

    t0

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+  // returns a Promise
+  const aPromise = new Promise((resolve, reject) => {
+    // no async operation provided intentionally
+  });
+  return aPromise;
+}
