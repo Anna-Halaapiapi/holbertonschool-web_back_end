3bd24d37d7a8597fe31e14861fa4be4de7ad9742
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-01-01 11:52:31 +1100

    t9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList () {
+  //  returns a Map of groceries with specified items (name, quantity)
+  const groceriesMap = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Bananas', 5]
+  ]);
+  return groceriesMap;
+}
