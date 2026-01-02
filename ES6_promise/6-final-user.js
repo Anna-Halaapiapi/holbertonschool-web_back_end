fe478f453a7b910e4345e4c9caf3a6e9dd6e9642
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-01-02 18:16:20 +1100

    t6 - fix import

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,4 +1,4 @@
-import { uploadPhoto } from "./utils";
+import uploadPhoto from "./5-photo-reject";
 import signUpUser from "./4-user-promise";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
@@ -7,7 +7,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     .then((results) => {
       return results.map((result) => {
         if (result.status === 'rejected') { // for rejected status - change reason to value
-          return {status: result.status, value: result.reason};
+          return {status: result.status, value: result.reason.toString()};
       } else { // for fulfilled status - keep same as obj in results arr
           return {status: result.status, value: result.value};
       }
