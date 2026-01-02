4515ec2bf4a2ff3d1345c463ca111d898591aace
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-01-02 18:12:19 +1100

    t6 - return new map array

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   // settles promises from called functions, and returns array in required format
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => {
-      results.map((result) => {
+      return results.map((result) => {
         if (result.status === 'rejected') { // for rejected status - change reason to value
           return {status: result.status, value: result.reason};
       } else { // for fulfilled status - keep same as obj in results arr
