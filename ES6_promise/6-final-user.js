e0b6893f9cd762e5898bfcdd5863712e45914a6a
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-01-02 18:10:05 +1100

    t6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,5 +3,14 @@ import signUpUser from "./4-user-promise";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   // settles promises from called functions, and returns array in required format
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      results.map((result) => {
+        if (result.status === 'rejected') { // for rejected status - change reason to value
+          return {status: result.status, value: result.reason};
+      } else { // for fulfilled status - keep same as obj in results arr
+          return {status: result.status, value: result.value};
+      }
+    });
+  });
 }
