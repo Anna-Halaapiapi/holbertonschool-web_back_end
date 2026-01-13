b341f013b7b7582930c68b617c14a30f49d0386c
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-01-13 19:18:25 +1100

    t1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -8,11 +8,11 @@ process.stdin.on('data', (chunk) => {
   const name = chunk.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
 
-// 3. if name  is piped display exit msg
-if (!process.stdin.isTTY) {
-  process.stdout.write('This important software is now closing\n');
-};
+  // 3. if name  is piped display exit msg
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+  }
 
-// 4. force close to prevent hang
-process.exit(0);
+  // 4. force close to prevent hang
+  process.exit(0);
 });
