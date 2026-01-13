0fde8dbed45326cab623b7395317c61608a595dd
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-01-13 19:24:17 +1100

    t1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,16 @@
 // node program that interacts with CLI
 
-// 1. displays welcom message
+// 1. display welcome
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// 2. read piped or typed name and print
-process.stdin.on('data', (chunk) => {
-  const name = chunk.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+// 2. read available input from stdin (typed or piped)
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
+});
 
-  // 3. if name  is piped display exit msg
-  if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n');
-  }
-
-  // 4. force close to prevent hang
-  process.exit(0);
+// 3. handle EOF or end of pipe
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
