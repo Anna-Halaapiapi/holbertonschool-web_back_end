8d5c93626cede75b481f26bd55237138f2993f8f
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-01-13 19:16:42 +1100

    t1 refactor

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,24 +1,18 @@
 // node program that interacts with CLI
+
 // 1. displays welcom message
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-let name = '';
-let printedFlag = false;
-
-// 2. reads user inputted name (both typed and piped)
+// 2. read piped or typed name and print
 process.stdin.on('data', (chunk) => {
-  name += chunk;
-  // 3. print name regardless of EOF or end of pipe reached to prevent hanging in test
-  if (!printedFlag && name.includes('\n')) {
-    console.log(`Your name is: ${name.trim()}`);
-    printedFlag = true;
-  }
-});
+  const name = chunk.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
 
-// 4. handle EOF or end of pipe reached
-process.stdin.on('end', () => {
-  if (!process.stdin.isTTY) {
-    // if name was piped - print name here with exit message
-    console.log('This important software is now closing');
-  }
+// 3. if name  is piped display exit msg
+if (!process.stdin.isTTY) {
+  process.stdout.write('This important software is now closing\n');
+};
+
+// 4. force close to prevent hang
+process.exit(0);
 });
