e65d8c30e47ae9454b9534e0cf698a6d5e91f2a0
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-01-13 18:59:06 +1100

    t1 - fix hanging checker test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,21 +2,23 @@
 // 1. displays welcom message
 console.log('Welcome to Holberton School, what is your name?');
 
-// 2. reads user inputted name (both typed and piped)
 let name = '';
+let printedFlag = false;
+
+// 2. reads user inputted name (both typed and piped)
 process.stdin.on('data', (chunk) => {
   name += chunk;
-  // if name was typed in interactive mode - print name
-  if (process.stdin.isTTY) {
+  // 3. print name regardless of EOF or end of pipe reached to prevent hanging in test
+  if (!printedFlag && name.includes('\n')) {
     console.log(`Your name is: ${name.trim()}`);
+    printedFlag = true;
   }
 });
 
-// 3. handle EOF or end of pipe reached
+// 4. handle EOF or end of pipe reached
 process.stdin.on('end', () => {
   if (!process.stdin.isTTY) {
     // if name was piped - print name here with exit message
-    console.log(`Your name is: ${name.trim()}`);
     console.log('This important software is now closing');
   }
 });
