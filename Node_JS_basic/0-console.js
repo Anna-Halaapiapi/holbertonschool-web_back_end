b5f8c0caef1d1076e163947e7236a6e6f2e64368
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-01-13 16:52:19 +1100

    t0 - amend for lint test

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,6 @@
-module.exports = displayMessage;
-
 function displayMessage(arg) {
+  // prints string argument to STDOUT using node in main
   console.log(arg);
 }
+
+module.exports = displayMessage;
