6b8d54f8d0bf94d313a0ab84869c07d24512e13a
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2026-01-15 13:52:45 +1100

    t7 - add comments

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -3,8 +3,8 @@ const express = require('express');
 const fs = require('node:fs/promises');
 
 // ==== HELPER FUNCTION ==== //
+// reads file asynchronously - returns error message OR student summary as string
 async function countStudents(filePath) {
-  // reads file asynchronously - returns error message OR student summary as string
   try {
     const data = await fs.readFile(filePath, 'utf8');
     const lines = data.trim().split(/\r?\n/);
@@ -36,28 +36,30 @@ async function countStudents(filePath) {
     }
     return summary.trim(); // upon success - return summary of students as string
   } catch (err) {
-    throw new Error('Cannot load the database'); // upoon failure - return err msg
+    throw new Error('Cannot load the database'); // upon failure - return err msg
   }
 }
 
 // === EXPRESS HTTP SERVER === //
-// 1. get database from command line
-const database = process.argv[2];
+const database = process.argv[2]; // get database from command line
 const app = express(); // create instance of express application
 const port = 1245; // define port that server will listen on
 app.get('/', (request, response) => { // routing for '/'
-  response.set('Content-Type', 'text/plain'); // returns plain text
-  response.send('Hello Holberton School!');
+  response.statusCode = 200; // OK
+  response.set('Content-Type', 'text/plain'); // expect plain text
+  response.end('Hello Holberton School!');
 });
 
 app.get('/students', (async (request, response) => { // routing for '/students'
-  response.set('Content-Type', 'text/plain'); // returns plain text
+  response.set('Content-Type', 'text/plain'); // expect plain text
   response.write('This is the list of our students\n');
   try {
-    const result = await countStudents(database);
-    response.end(result); // send student summary from helper func
+    const result = await countStudents(database); // await helper's return before using in response
+    response.statusCode = 200; // OK
+    response.end(result); // send student summary from helper
   } catch (err) {
-    response.end(err.message);
+    response.statusCode = 500; // statuscode for generic database error
+    response.end(err.message); // send err msg from helper
   }
 }));
 app.listen(port, () => { // start server and listen for incoming requests
